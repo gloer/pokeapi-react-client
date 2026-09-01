53b5a84d96fe51e025071708da86981922f79c26
gloer <gloer@langslet.com>
2026-09-01 13:39:25 +0200

    added id with crypto.randomUUID

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -2,17 +2,31 @@ import { useState } from "react";
 import "./SearchForm.css";
 import SearchResults from "../SearchResults/SearchResults";
 
+type Search = {
+  id: string;
+  value: string;
+};
+
 export default function SearchForm() {
 
   const [results, setResults] = useState(null);
   const [userHasSearched, setUserHasSearched] = useState(false);
+  const [searchHistory, setSearchHistory] = useState<Search[]>([]);
 
   async function findPokemon(formData: FormData) {
-    const q = String(formData.get("search-text"));
     setUserHasSearched(true);
+    
+    const q = String(formData.get("search-text"));  
+    const newSearch = { id: crypto.randomUUID(), value: q};  
+    let newHistory = [newSearch, ...searchHistory];
+    
+    if(newHistory.length > 3) {
+      newHistory = newHistory.slice(0,-1);
+    }
+    setSearchHistory(newHistory);
+
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${q}`);
-    if(response.status === 404 || response.status === 400) {
-      console.log(response.status);
+    if(response.status === 404 || response.status === 400) {      
       setResults(null);      
     } else {
       const data = await response.json();      
@@ -21,7 +35,7 @@ export default function SearchForm() {
   }
 
   return (
-    <>
+    <div>
       <form 
         className="search-form"
         action={findPokemon}
@@ -30,10 +44,17 @@ export default function SearchForm() {
         <input 
           type="text" 
           id="search-text" 
-          name="search-text"          
+          name="search-text"   
+          required       
         />
         <button type="submit">Finn!</button>
       </form>
+      <hr/>
+      <p style={{display: "flex", gap: "0.5rem"}}>Siste søkeord: {
+          searchHistory.map(s => <span key={s.id}>Id: {s.id} - {s.value}</span>)
+        }
+      </p>
+      <hr/>
       <div className="search-results">
         {results === null && userHasSearched && (
           <>
@@ -48,6 +69,6 @@ export default function SearchForm() {
         )
       }
       </div>
-    </>
+    </div>
   );
 }
--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,9 +1,30 @@
+type Ability = {
+    name: string;
+    url: string;
+};
+
+type AbilityProps = {
+    is_hidden: boolean;
+    slot: number;
+    ability: Ability;
+}
+
+type HeldItem = {
+    item: {
+        name: string;
+        url: string;
+    };
+};
+
+
 type Results = {
     name: string;
     base_experience: number;
     height: number;
     weight: number;
-};
+    abilities: AbilityProps[];
+    held_items: HeldItem[];
+}
 
 type SearchResultsProps = {
     results: Results;
@@ -19,13 +40,31 @@ Held items
 */
 
 export default function SearchResults({results} : SearchResultsProps ) {    
-    const {name, base_experience, height, weight} = results;
+    const {name, base_experience, height, weight, abilities, held_items} = results;
+    if(name) {
+        return (
+            <article>
+                <p>Name: {name}</p>
+                <p>Base xp: {base_experience}</p>
+                <p>Height: {height}</p>
+                <p>Weight: {weight}</p>
+                <h2>Abilities</h2>
+                <section>
+                    {                    
+                        abilities.map(ability => <p key={`${name}${ability.ability.name}`}>{ability.ability.name}</p>)
+                    }
+                </section>
+                <h2>Held items</h2>
+                <section>                    
+                    {
+                        held_items.length > 0 ? held_items.map(item => <p key={`${name}${item.item.name}`}>{item.item.name}</p>)
+                        : <p>No held items</p>
+                    }
+                </section>
+            </article>
+        );
+    }
     return (
-        <article>
-            <p>Name: {name}</p>
-            <p>Base xp: {base_experience}</p>
-            <p>Height: {height}</p>
-            <p>Weight: {weight}</p>
-        </article>
+        <p>Ingen resultater funnet</p>
     );
 }
